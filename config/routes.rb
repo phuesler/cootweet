4c37ba64215dceefb1c84101fb4f3655b90d1d2f
Patrick Huesler <patrick.huesler@gmail.com>
2009-07-07 15:40:30 +0200

    beautified start page a little

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'application', :action => 'index'
   map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'
   map.login 'login', :controller => 'user_sessions', :action => 'new'
+  map.signup 'signup', :controller => 'users', :action => 'new'
   map.resources :user_sessions
   map.resources :users 
   map.resources :tweets

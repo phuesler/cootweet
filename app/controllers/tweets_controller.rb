e8dcae3b9dfb12238409b4f8f5152ff9eb1bfdb9
Patrick Huesler <patrick.huesler@gmail.com>
2009-07-07 15:51:51 +0200

    fixed bug in tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,19 +1,25 @@
 class TweetsController < ApplicationController
   before_filter :require_user
+  before_filter :get_tweets
   
   def index
     @tweet = Tweet.new
-    @tweets = current_user.tweets.paginate(:page => params[:page], :per_page => 10)
   end
   
   def create
     @tweet = current_user.tweets.build(params[:tweet])
     if @tweet.save
-      flash[:notice] = "Tadaaaa"
+      flash[:notice] = "Tweet saved!"
       redirect_to tweets_path
     else
-      flash[:error] = "There was an error"
+      flash[:error] = "Tweet could not be saved!"
       render :action => :index
     end
+  end
+  
+  protected
+  
+  def get_tweets
+    @tweets = current_user.tweets.paginate(:page => params[:page], :per_page => 10)    
   end
 end
